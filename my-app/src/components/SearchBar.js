841a8bbff7cfb92d099b0ccabf36acf08fca0099
NickdevC <nick_coles@msn.com>
2023-04-01 15:03:49 +0000

    Add filter method to handleChange function

--- my-app/src/components/SearchBar.js
+++ my-app/src/components/SearchBar.js
@@ -9,13 +9,27 @@ export class SearchBar extends Component {
             names: importedNames
         }
     }
+    handleChange = (event) => {
+        const inputText = event.target.value.toLowerCase()
+        const filteredNames = importedNames.filter(name => {
+            return name.toLowerCase().includes(inputText)
+        })
+        this.setState({
+            names: filteredNames
+        })
+    }
     render() {
         return (
             <div>
                 <h1>Name search:</h1>
                 <p>Matching names found: {this.state.names.length}</p>
                 <form>
-                    <input type="text" name='name' placeholder='search names...'>
+                    <input 
+                        type="text" 
+                        name='name' 
+                        placeholder='search names...'
+                        onChange={(event) => this.handleChange(event)}
+                    >
                     </input>
                 </form>
                 <div style={{margin: 'auto'}}>
